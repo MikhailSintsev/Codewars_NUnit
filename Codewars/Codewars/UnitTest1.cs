6161a96f53fd7e15b4e1f27d1946650f55a31b3c
Mikhail Sintsev <mikhail.sintsev@gmail.com>
2021-02-16 12:48:29 +0400

    Add instuction for  "Square_Every_Digit"

--- Codewars/Codewars/UnitTest1.cs
+++ Codewars/Codewars/UnitTest1.cs
@@ -3,6 +3,10 @@ using NUnit.Framework;
 
 namespace Square_Every_Digit
 {
+    // Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+    // For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+    // Note: The function accepts an integer and returns an integer
+
     public class Kata
     {
         public static int SquareDigits(int n)
