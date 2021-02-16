1ee775725148f46182fe3e6a75e409f3e829267c
Mikhail Sintsev <mikhail.sintsev@gmail.com>
2021-02-16 12:39:02 +0400

    Added solution "Square_Every_Digit"

--- Codewars/Codewars/UnitTest1.cs
+++ Codewars/Codewars/UnitTest1.cs
@@ -1,18 +1,30 @@
+using System;
 using NUnit.Framework;
 
-namespace Codewars
+namespace Square_Every_Digit
 {
-    public class Tests
+    public class Kata
     {
-        [SetUp]
-        public void Setup()
+        public static int SquareDigits(int n)
         {
-        }
+            string result = "";
 
+            foreach (char item in n.ToString())
+            {
+                int multiplier = int.Parse(item.ToString());
+                result += (multiplier * multiplier);
+            }
+            return int.Parse(result);
+        }
+    }
+
+    [TestFixture]
+    public class KataTest
+    {
         [Test]
-        public void Test1()
+        public void SquareDigitsTest()
         {
-            Assert.Pass();
+            Assert.AreEqual(811181, Kata.SquareDigits(9119));
         }
     }
 }
